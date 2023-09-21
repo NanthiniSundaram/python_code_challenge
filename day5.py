4f538a8e97bbb7d1393b14c043ac34a2dfbdaaa4
NanthiniSundaram <135304038+NanthiniSundaram@users.noreply.github.com>
2023-09-21 12:12:22 +0530

    Create day5.py
    Built my very first Akinator 🧞‍♂️ style game with 'if' statements! Day 5 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day5.py
@@ -0,0 +1,29 @@
+#challenge day_5
+
+print("Marvel movie character creator!!")
+
+hang=input("Do you like 'hanging around' >")
+if hang=="yes":
+  print("Hii! You are spider man!!")
+else:
+  print("You are not spider man!")
+voice=input("Do you have a 'gravelly' voice >")
+if voice=="yes":
+  print("Hii! You are korg")
+else:
+  print("You are not korg")
+feel=input("Do you feel 'marvelous'>")
+if feel=="yes":
+  print("Hii! You are Captain Marvel!!")
+else:
+  print("Oops! You are not captain Marvel!")
+vision=input("Do you have ghost seeing ability >")
+if vision=="yes":
+  print("Hii! You are hulk!!")
+else:
+  print("You are not hulk")
+person=input("Are you a leader >")
+if person=="yes":
+  print("Hii!! You are Captain America!!")
+else:
+  print("Oops! You are not Captain America!")
