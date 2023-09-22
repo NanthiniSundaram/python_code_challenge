df25027226e15451706b74a0b8d5cbb2f31ec141
NanthiniSundaram <135304038+NanthiniSundaram@users.noreply.github.com>
2023-09-22 12:01:48 +0530

    Create day6.py
    What the elif did I build today? A hack proof login system! Try breaking in 👾!! Day 6 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day6.py
@@ -0,0 +1,29 @@
+#challenge day_6
+
+print("***LOGIN SYSTEM***")
+print("***")
+name=input("Enter your name >")
+password=input("Enter your password >")
+if name=="Tom" and password==password:
+  print("""Welcome tom!! , Your login is successful!! It's a great pleasure that you have interest in participating in the event.
+  Have a great day!!
+  
+  Don't forget to give your experience in the feedback!!""")
+elif name=="Jerry" and password==password:
+  print("""Welcome Jerry!! , Your login is successful!! It's a great pleasure that you have interest in participating in the event.
+  Have a great day!!
+  
+  Don't forget to give your experience in the feedback!""")
+elif name=="Harry" and password==password:
+  print("""Welcome Harry!! , Your login is successful!! It's a great pleasure that you have interest in participating in the event.
+  Have a great day!!
+  
+  Don't forget to give your experience in the feedback!""")
+elif name=="Jefry" and password==password:
+  print("""Welcome Jefry!! , Your login is successful!! It's a great pleasure that you have interest in participating in the event.
+  Have a great day!!
+  
+  Don't forget to give your experience in the feedback!""")
+else:
+  print("""Oops! login failed!!
+  Try to Sign up..""")
