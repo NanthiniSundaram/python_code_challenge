bd04e28f93520544bf8f90182164c2fa8827bded
NanthiniSundaram <135304038+NanthiniSundaram@users.noreply.github.com>
2023-09-25 09:24:33 +0530

    Create day8.py
    Sending out good vibes 😎 with the affirmation generator! Day 8 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day8.py
@@ -0,0 +1,27 @@
+#challenge_day8
+
+print("-----CUSTOM AFFIRMATION GENERATOR-----")
+name=input("Enter your name >")
+fav=input("Enter your favourite activity >")
+print("Hello!!",name,"Welcome to this custom affirmation generator!!")
+day=input("Enter the day of the week >")
+if day=="Monday" or day=="monday":
+  print("""Hii,you are commited to  change your Monday mindset. You will start each week with gratitude, motivation, and a renewed sense of energy.""")
+elif day=="Tuesday" or day=="tuesday":
+  print("""Every single thing that happens in your life has been leading you towards better Tuesday.Every Tuesday is a new opportunity to start over again.""")
+elif day=="Wednesday" or day=="wednesday":
+  print("""You are supercharged to get yourself at work this Wednesday.You are excited about the success you shall achieve in your work this Wednesday.You are going to prepare hard for this Wednesday so that you can reap the benefits on Thursday""")
+elif day=="Thursday" or day=="thursday":
+  print("""Every Thursday you should choose happiness over negativity.Every Thursday is a new chance to begin again.
+Every Thursday is a new opportunity to live your best life.
+Every Thursday that you shall overcome is meant for your growth.""")
+elif day=="Friday" or day=="friday":
+  print("""As the week comes to a close, you can reflect on your Friday with thankfulness and happiness.Every Friday is fine.
+Every Friday is precisely how it should be.Every Friday, you get better and better.""")
+elif day=="Saturday" or day=="saturday":
+  print("""All is well in  life this Saturday.You are taking a pause to stop worrying and enjoy life today. You release all stress that you accumulated over the week.You are determined to relax and rest without feeling guilty.You're gonna have a ton of reasons to smile this Saturday!.""")
+else:
+  print("""Ohh!!It's Sunday.Today is a good day to be the best version of myself. Make an effort to be more present and enjoy every moment.Trust the Universe to guide your way to success.Be grateful that you're alive, well, and ready for new beginnings.""")
+  
+print("Thank you!!",name,"for using this custom affirmation generator.Wishing all success for you !!")
+  
