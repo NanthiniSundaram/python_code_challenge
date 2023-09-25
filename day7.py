04576a81062feb9bbbbe04022db642e4a8b49f4b
NanthiniSundaram <135304038+NanthiniSundaram@users.noreply.github.com>
2023-09-25 09:23:05 +0530

    Create day7.py
    With some nifty nesting 🐣 skills I built a game to test if you are a true fan of a TV show 📺 ! Day 7 of #Replit100DaysOfCode #100DaysOfCode. Join me on @Replit https://join.replit.com/python

---
+++ day7.py
@@ -0,0 +1,37 @@
+#challenge_day7
+
+print("**Fake Fan Finder**")
+print("--------------------")
+anime=input("What is your favourite anime >")
+if anime=="One piece" or "one piece":
+  char=input("Oh really!! What is your favourite character >")
+  if char=="Nami" or "nami":
+    job=input("Okay then, What is her job on the ship >")
+    if job=="navigator":
+      print("ohh!! congrats,,you got it right")
+    else:
+      print("See you are fake one piece fan")
+  elif char=="Luffy" or "luffy":
+    job=input("Okay then,what is the job of luffy >")
+    if job=="pirate":
+      print("Congrats!! You got it right")
+    else:
+      print("See you are a fake fan")
+  else:
+    print("Ohh!! thats good")
+elif anime=="naruto":
+  char=input("Oh really!! What is your favourite character >")
+  if char=="minato":
+    job=input("Okay then,what does minato do >")
+    if job=="hokage":
+      print("Congrats!! You got it right")
+    else:
+      print("See you are fake one piece fan")
+  else:
+    print("Ohh!! that's a good choice") 
+else:
+  print("Ohh!! That's a great choice!")
+    
+  
+    
+
